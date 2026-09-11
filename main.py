422c0a0a70f5910028a2044d25ec5a55d2429fe9
ludwigcano1 <st.ludwig.cuenta@gmail.com>
2026-09-11 12:47:30 -0500

    subiendo requirements

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
-@st.cache_data
-def cargar_datos():
-    ddd = pd.read_excel("CARGAS_GENERAL.xlsx")
-    return ddd
-
-
-df_l = cargar_datos()
+df_l = pd.read_excel("CARGAS_GENERAL.xlsx")
 secciones = ["P01", "P02"]
 
 
